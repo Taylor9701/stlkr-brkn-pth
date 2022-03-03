d87b1cd7b4f3741af630c01a7238595628155bf2
ScratKnapp <hatdescript@yahoo.ca>
2022-03-02 21:42:51 -0600

    Tweaks Shakedown, and a grenade I forgot

--- garrysmod/gamemodes/ix_stalker/plugins/armor/items/armor/sh_bandit_seva.lua
+++ garrysmod/gamemodes/ix_stalker/plugins/armor/items/armor/sh_bandit_seva.lua
@@ -16,18 +16,18 @@ ITEM.isHelmet = true
 ITEM.isArmor = true
 ITEM.radProt = 0.3
 ITEM.overlayPath = "vgui/overlays/hud_sci"
-ITEM.artifactcontainers = {"4"}
+ITEM.artifactcontainers = {"5"}
 ITEM.img = Material("vgui/hud/seva.png")
 ITEM.weight = 10
 ITEM.maxArmor = 10
 ITEM.res = {
-	["Bullet"] = 0.3,
-	["Blast"] = 0.3,
+	["Bullet"] = 0.1,
+	["Blast"] = 0.1,
 	["Fall"] = 0.1,
 	["Burn"] = 0.2,
 	["Radiation"] = 0.3,
 	["Chemical"] = 0.3,
-	["Shock"] = 0.2,
+	["Shock"] = 0.1,
 	["Psi"] = 0.1,
 }
 
