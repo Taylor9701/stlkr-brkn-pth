d672bc17e76b11bd2499b8e0034e554a00cdd43a
ScratKnapp <hatdescript@yahoo.ca>
2022-03-22 18:48:34 -0500

    Adds new armor, upgrades, parts, Pavuk bits, etc

--- garrysmod/gamemodes/ix_stalker/schema/items/junk/sh_b5parts.lua
+++ garrysmod/gamemodes/ix_stalker/schema/items/junk/sh_b5parts.lua
@@ -1,6 +1,6 @@
 ITEM.name = "Berillium-enriched Parts (Berill-5M)"
 ITEM.model = "models/kek1ch/armor_repair_pro.mdl"
-ITEM.description = "A package containing some sort of military gear, markings insist the contents are toxic to inhale and ingest. 3 of these and a Berkut suit could result in something useful."
+ITEM.description = "A package containing some sort of military gear, markings insist the contents are toxic to inhale and ingest. 3 of these and a Beryl-3 or Berkut suit could result in something useful."
 ITEM.category = "Parts"
 ITEM.flag = "A"
 ITEM.price = 21000
