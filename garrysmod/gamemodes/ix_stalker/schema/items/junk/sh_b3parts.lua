b0182a2d4638c48432ea90281180f31728474a94
ScratKnapp <hatdescript@yahoo.ca>
2022-03-22 18:53:14 -0500

    Lowers B3 part price

--- garrysmod/gamemodes/ix_stalker/schema/items/junk/sh_b3parts.lua
+++ garrysmod/gamemodes/ix_stalker/schema/items/junk/sh_b3parts.lua
@@ -3,6 +3,6 @@ ITEM.model = "models/kek1ch/armor_repair_pro.mdl"
 ITEM.description = "A package containing a steel helmet and some improved kevlar, meant for upgrading an old suit to a more modern standard. 2 of these and a Beryl-1 could result in something interesting."
 ITEM.category = "Parts"
 ITEM.flag = "A"
-ITEM.price = 12000
+ITEM.price = 10000
 ITEM.width = 1
 ITEM.height = 1
