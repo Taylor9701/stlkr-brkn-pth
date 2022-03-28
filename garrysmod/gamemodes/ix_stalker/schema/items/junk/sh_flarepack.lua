4c3e9503d77382f0ece484a139633b7fdb5e9d58
ScratKnapp <hatdescript@yahoo.ca>
2022-03-27 23:33:43 -0500

    Fixes

--- garrysmod/gamemodes/ix_stalker/schema/items/junk/sh_flarepack.lua
+++ garrysmod/gamemodes/ix_stalker/schema/items/junk/sh_flarepack.lua
@@ -1,5 +1,5 @@
 ITEM.name = "Emergency Flares"
-ITEM.model = "models/items/boxflares.mdl"
+ITEM.model = "models/kali/props/metro/boxes/box_g.mdl"
 ITEM.description = "A pack of five emergency flares, useful for creating light where light is limited, or as a distress signal. Emits light in a 10 meter radius."
 ITEM.category = "junk"
 ITEM.flag = "1"
