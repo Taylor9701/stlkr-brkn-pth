a11249834245555c5d2ee7701e0959e3761c9b39
ScratKnapp <hatdescript@yahoo.ca>
2022-04-03 15:02:57 -0500

    Adds MORE fucking rocks, fixes some

--- garrysmod/gamemodes/ix_stalker/plugins/artifacts/items/artifacts/sh_backbone.lua
+++ garrysmod/gamemodes/ix_stalker/plugins/artifacts/items/artifacts/sh_backbone.lua
@@ -1,7 +1,7 @@
 ITEM.name = "Backbone"
-ITEM.model ="models/predatorcz/stalker/artifacts/psi_field.mdl"
+ITEM.model = "models/predatorcz/stalker/artifacts/psi_field.mdl"
 ITEM.description = "A flaccid spine-like artifact."
-ITEM.longdesc = "A flaccid spine-like artifact that stiffens upon contact with certain chemical agents, pretty viscous and has the property of periodically lowering the temperature of the surrounding air around itself. Emits radiation. \n\n+3p Chemical\n+3p Thermal\n +3 Rads"
+ITEM.longdesc = "A flaccid spine-like artifact that stiffens upon contact with certain chemical agents, pretty viscous and has the property of periodically lowering the temperature of the surrounding air around itself. Emits radiation. \n\n+2p Chemical\n+2p Thermal\n +2 Rads"
 ITEM.width = 1
 ITEM.height = 1
 ITEM.price = 16000
