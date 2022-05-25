99a3ea65fece0ae5c5493b9ecfd75f24b3c55fe9
ScratKnapp <hatdescript@yahoo.ca>
2022-05-25 15:13:19 -0500

    Up the grenade prices

--- garrysmod/gamemodes/ix_stalker/plugins/cwweapons/items/ammunition/sh_m203rounds.lua
+++ garrysmod/gamemodes/ix_stalker/plugins/cwweapons/items/ammunition/sh_m203rounds.lua
@@ -1,7 +1,7 @@
 ITEM.name = "M203 Grenade"
 ITEM.model = ("models/weapons/upgrades/a_projectile_m203.mdl")
 ITEM.description = "An M203 Grenade, made specifically to be launched from NATO Grenade Launchers."
-ITEM.price = 5000
+ITEM.price = 9000
 ITEM.flag = "3"
 ITEM.category = "Ammunition"
 ITEM.ammo = "40MM"
