afd2cbe8d4add786e72bf4405cf05685f5d8b77e
LilithSanders <83739216+LilithSanders@users.noreply.github.com>
2022-05-29 02:11:33 -0500

    New Barska Model

--- garrysmod/gamemodes/ix_stalker/plugins/cwweapons/items/attachments/sh_barska.lua
+++ garrysmod/gamemodes/ix_stalker/plugins/cwweapons/items/attachments/sh_barska.lua
@@ -1,7 +1,7 @@
 ITEM.name = "Barska Electro Sight"
 ITEM.description = "A panoramic sight built to reduce eye strain and provide a large field of view within the sight, the Barksa series is favored for its tinted lenses and practicality."
 ITEM.category = "Attachments"
-ITEM.model = "models/cw2/attachments/microt1.mdl"
+ITEM.model = "models/silver/stalker/items/a_barska.mdl"
 ITEM.width = 1
 ITEM.height = 1
 ITEM.price = 6500
