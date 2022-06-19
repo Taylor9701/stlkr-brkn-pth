4f19979a8c37178c82fe30d1e6775fe1ddb5e024
ScratKnapp <hatdescript@yahoo.ca>
2022-06-19 14:21:49 -0500

    "Reverts pellicle test, should work fine now"

--- garrysmod/gamemodes/ix_stalker/plugins/artifacts/items/artifacts/sh_pellicle.lua
+++ garrysmod/gamemodes/ix_stalker/plugins/artifacts/items/artifacts/sh_pellicle.lua
@@ -1,4 +1,4 @@
-ITEM.name = "Pellicle Test"
+ITEM.name = "Pellicle"
 ITEM.model ="models/tnb/stalker/artifacts/witchesjelly.mdl"
 ITEM.description = "Spherish, blue artifact."
 ITEM.longdesc = "Many researchers believe this artifact should be incapable of forming in any natural environment. It emits a slightly acidic compound which seems to neutralize harmful chemicals. It is the first artifact to be reproduced in laboratory conditions.\n\nChemical +3"
