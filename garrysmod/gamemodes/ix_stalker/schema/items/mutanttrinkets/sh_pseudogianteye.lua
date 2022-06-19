c9d83191ed024d051489fff6552fa4bd51586a1a
Zopples <106275005+Zopples@users.noreply.github.com>
2022-06-19 17:14:05 -0500

    Create sh_pseudogianteye.lua

---
+++ garrysmod/gamemodes/ix_stalker/schema/items/mutanttrinkets/sh_pseudogianteye.lua
@@ -0,0 +1,11 @@
+ITEM.name = "Pseudogiant Eye"
+ITEM.model ="models/stalker/outfit/bandit3.mdl"
+ITEM.description = "The gigantic eye of a Pseudogiant. Kinda creepy."
+ITEM.longdesc = "The gigantic, wet, rubbery eye of a Pseudogiant. Heavy and smelling horribly, the only real use is selling to the Ecologists, or if you're in need of a kickball."
+ITEM.width = 2
+ITEM.height = 2
+ITEM.price = 10000
+ITEM.flag = "1"
+ITEM.value = ITEM.price*1*1
+ITEM.weight = 3
+ITEM.quantity = 1
