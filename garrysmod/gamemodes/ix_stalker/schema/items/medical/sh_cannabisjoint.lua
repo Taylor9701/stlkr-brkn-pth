4a95d765c79b3b78af0f8f09eae6975b3e87edd6
ScratKnapp <hatdescript@yahoo.ca>
2022-07-03 20:22:53 -0500

    Fixed weed

--- garrysmod/gamemodes/ix_stalker/schema/items/medical/sh_cannabisjoint.lua
+++ garrysmod/gamemodes/ix_stalker/schema/items/medical/sh_cannabisjoint.lua
@@ -1,7 +1,7 @@
 ITEM.name = "Cannabis Joint"
 ITEM.description = "A joint of weed, complete with a filter."
 ITEM.longdesc = "It smells wacky, weighs about a gram, makes your throat dry, your eyes watery, and stupid things more funny when you smoke it. A general system depressant, it can be used to help aid exposure to psi either before or after, though it may make your time to react a bit slower or make you take a little bit longer due to your impaired state. Lasts 30 minutes.\n\n +1 Psi\n -5/10 to rolls"
-ITEM.model = "models/kek1ch/drink_joint.mdl"
+ITEM.model = "models/ethprops/consumable/joint.mdl"
 ITEM.width = 1
 ITEM.height = 1
 ITEM.category = "Medical"
