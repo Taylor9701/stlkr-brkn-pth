1d175538700eb15b6f42a1262b205fab8fa3cf4f
ScratKnapp <hatdescript@yahoo.ca>
2022-07-19 16:04:42 -0500

    New stuff

--- garrysmod/gamemodes/ix_stalker/schema/items/medical/sh_antidote.lua
+++ garrysmod/gamemodes/ix_stalker/schema/items/medical/sh_antidote.lua
@@ -2,7 +2,7 @@ ITEM.name = "Antidote"
 ITEM.model = "models/kek1ch/drug_antidot.mdl"
 ITEM.description = "A combination drug that counter-acts poisoning and chemical weapons inhalation. +1 Chem protection tier (Max 2) for 1 hour and resets Chem hotspot. Makes user experience vertigo at random points, making climbing and acrobatic stunts impossible."
 ITEM.category = "Medical"
-ITEM.price = 6000
+ITEM.price = 3000
 ITEM.flag = "1"
 
 ITEM.functions.Use = {
