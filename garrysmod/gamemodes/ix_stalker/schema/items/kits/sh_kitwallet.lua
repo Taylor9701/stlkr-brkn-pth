b9f67f3a70069096ac77fde8943eabf7983676bf
ScratKnapp <hatdescript@yahoo.ca>
2022-08-12 14:24:09 -0500

    More stuff + Cash Stash

--- garrysmod/gamemodes/ix_stalker/schema/items/kits/sh_kitwallet.lua
+++ garrysmod/gamemodes/ix_stalker/schema/items/kits/sh_kitwallet.lua
@@ -1,25 +1,27 @@
-ITEM.name = "Test Walllet"
-ITEM.description= "Default kit."
-ITEM.longdesc = ""
-ITEM.model = "models/props_junk/garbage_bag001a.mdl"
+ITEM.name = "Cash Stash"
+ITEM.description= "A bundle of rubles bound together with a rubber band."
+ITEM.longdesc = "You'll need to count it to see how much is bound together."
+ITEM.model = "models/illusion/eftcontainers/roubles.mdl"
 ITEM.width = 1
 ITEM.height = 1
 ITEM.category = "Kits"
 ITEM.itemsForcedDrop = {}
 ITEM.items[1] = {
 	dropAmount = 1, --How many times it will roll for drop
-	dropRareChance = 40, --chances for the rarer drops
-	dropVeryRareChance = 20,
+	dropRareChance = 30, --chances for the rarer drops
+	dropVeryRareChance = 15,
 	itemsCommon = {
 		{"bagrubles", {["quantity"] = 100}}, 
 		{"bagrubles", {["quantity"] = 200}},
+		{"bagrubles", {["quantity"] = 500}},
 	},
 	itemsRare = {
 		{"bagrubles", {["quantity"] = 1000}}, 
-		{"762x25"},
+		{"bagrubles", {["quantity"] = 2500}}, 
+
 	},
 	itemsVeryRare = {
-		{"bagrubles", {["quantity"] = 5000}}, 
+		{"bagrubles", {["quantity"] = 6000}}, 
 	}
 
 
