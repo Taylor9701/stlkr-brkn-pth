f40b6ea45121c6337731b85df26060132b6e76b9
log <lwayne@fullsail.edu>
2022-12-05 16:41:12 -0500

    Medical & consumable quantity fixes
    Maximum quantity is now a value of the base class and needed to be defined in each item which didn't previously define it.
    
    Some items appeared to be attributed to the wrong base. Cigarettes, Cannabis Joint, and Beard Oil were moved to Consumables. EMT Bag was moved to junk.
    
    Psi Block used Psi and Psy interchangeably, it now consistently uses Psi.
    
    More changes to come.

---
+++ garrysmod/gamemodes/ix_stalker/schema/items/junk/sh_emtbag.lua
@@ -0,0 +1,9 @@
+ITEM.name = "EMT Bag"
+ITEM.model = "models/illusion/eftcontainers/grizzly.mdl"
+ITEM.longdesc = "A medic's large trauma care bag is enough to stabilize wounded in the field and deliver them to a medical post, where they can begin recovery."
+ITEM.description = "A kit-bag with various instruments for rapid life-saving purposes."
+ITEM.category = "Medical"
+ITEM.flag = "1"
+ITEM.price = 10000
+ITEM.width = 1
+ITEM.height = 1
